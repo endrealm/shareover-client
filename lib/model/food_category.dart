7e32bf1e1279315eecc029f4521a14c62d957ecc
Julia Duong <julia.duong@isys.de>
2023-05-26 11:04:14 +0200

    Added CreateOfferMenu + F00D Category

--- lib/model/food_category.dart
+++ lib/model/food_category.dart
@@ -1,16 +1,34 @@
+import 'package:flutter/material.dart';
+
 class FoodCategory {
   final String categoryId;
   final String displayName;
+  late final Icon icon;
 
-  FoodCategory({required this.categoryId, required this.displayName});
+  FoodCategory(
+      {required this.categoryId,
+      required this.displayName,
+      required IconData iconData}) {
+    icon = Icon(iconData);
+  }
 }
 
 final categories = [
-  FoodCategory(categoryId: "bread", displayName: "Bread"),
-  FoodCategory(categoryId: "dairy", displayName: "Dairy"),
-  FoodCategory(categoryId: "fruits", displayName: "Fruits"),
-  FoodCategory(categoryId: "vegetable", displayName: "Vegetable"),
-  FoodCategory(categoryId: "fish", displayName: "Fish"),
-  FoodCategory(categoryId: "meat", displayName: "Meat"),
-  FoodCategory(categoryId: "consumer_goods", displayName: "Consumer Goods"),
+  FoodCategory(
+      categoryId: "bread", displayName: "Bread", iconData: Icons.bakery_dining),
+  FoodCategory(categoryId: "dairy", displayName: "Dairy", iconData: Icons.egg),
+  FoodCategory(
+      categoryId: "fruits",
+      displayName: "Fruits",
+      iconData: Icons.auto_awesome),
+  FoodCategory(
+      categoryId: "vegetable", displayName: "Vegetable", iconData: Icons.grass),
+  FoodCategory(
+      categoryId: "fish", displayName: "Fish", iconData: Icons.set_meal),
+  FoodCategory(
+      categoryId: "meat", displayName: "Meat", iconData: Icons.directions_run),
+  FoodCategory(
+      categoryId: "consumer_goods",
+      displayName: "Consumer Goods",
+      iconData: Icons.storefront),
 ];
--- lib/pages/map/popups/create_offer.dart
+++ lib/pages/map/popups/create_offer.dart
@@ -1,39 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shareover/model/food_category.dart';
 
 class CreateOfferWidget extends StatelessWidget {
   final VoidCallback close;
+
   const CreateOfferWidget({Key? key, required this.close}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
         child: Container(
-          color: Theme.of(context).dialogBackgroundColor,
-          child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FloatingActionButton(
-                      onPressed: close,
-                      backgroundColor: Colors.green,
-                      child: const Icon(Icons.close),
-                    ),
-                  ],
-                ),
-                const Expanded(
-                  child: SizedBox.expand(
-                    child: CreateForm()
-                  ),
-                ),
-              ]
-          ),
-        )
-    );
+      padding: const EdgeInsets.all(8),
+      color: Theme.of(context).dialogBackgroundColor,
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: close,
+              backgroundColor: Colors.green,
+              child: const Icon(Icons.close),
+            ),
+          ],
+        ),
+        const Expanded(
+          child: SizedBox.expand(child: CreateForm()),
+        ),
+      ]),
+    ));
   }
 }
-
 
 class CreateForm extends StatefulWidget {
   const CreateForm({Key? key}) : super(key: key);
@@ -43,8 +40,23 @@ class CreateForm extends StatefulWidget {
 }
 
 class _CreateFormState extends State<CreateForm> {
-
   var selectedCategory = categories[0].categoryId;
+  late TextEditingController _controller;
+  late TextEditingController _amountController;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _amountController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,23 +65,66 @@ class _CreateFormState extends State<CreateForm> {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         const Text("Select a category"),
-        Wrap(
-          spacing: 5.0,
-          children: categories.map(
-                  (category) =>  ChoiceChip(
-                    label: Text(category.displayName),
-                    selected: category.categoryId == selectedCategory,
-                    onSelected: (bool selected) {
-                      setState(() {
-                        selectedCategory = category.categoryId;
-                      });
-                    },
-                  ),
-          ).toList(),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Wrap(
+            spacing: 6.0,
+            children: categories.map(
+              (category) {
+                var selected = category.categoryId == selectedCategory;
+                return ChoiceChip(
+                  avatar: !selected ? category.icon : const SizedBox.shrink(),
+                  label: Text(category.displayName),
+                  selected: selected,
+                  onSelected: (bool selected) {
+                    setState(() {
+                      selectedCategory = category.categoryId;
+                    });
+                  },
+                );
+              },
+            ).toList(),
+          ),
         ),
-
+        Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                child: TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Product Name",
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                keyboardType: TextInputType.number,
+                controller: _amountController,
+                // onTapOutside: (_) {
+                //   _amountController.text = int.parse(_amountController.text).toString();
+                // },
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Amount",
+                ),
+              ),
+            )
+          ],
+        ),
+        const Spacer(),
+        Row(children: [
+          Expanded(
+            child:
+                ElevatedButton(onPressed: () {}, child: const Text("Submit")),
+          )
+        ])
       ],
     );
   }
 }
-
