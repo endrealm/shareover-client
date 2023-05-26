f09ae07f0ce29f2bac7778ae4a2aed4b58ec44e0
endsmasher <endsmeshaa@gmail.com>
2023-05-26 17:12:19 +0200

    added setup page

---
+++ lib/utils/constants.dart
@@ -0,0 +1,18 @@
+
+import 'dart:ui';
+
+class Constants {
+
+  static const kPrimaryColor = Color(0xFF9E9E9E);
+  static const kBlackColor = Color(0xFF000000);
+  static const kDarkGreyColor = Color(0xFF9E9E9E);
+  static const kBorderColor = Color(0xFFEFEFEF);
+
+
+  static const textSignInTitle = "ShareOver";
+  static const textSmallSignIn = "Name";
+  static const textSmallSignIn2 = "Password";
+  static const textlabelname = "Eingabefeld";
+  static const textlabelpw = "Eingabefeld";
+
+}
